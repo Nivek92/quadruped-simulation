8e183583421278317b0675a2303d3f6d08ddf908
NiVeK92 <Ztear@web.de>
2020-09-28 16:01:25 +0200

    leg following bezier curve

--- src/bezier.rs
+++ src/bezier.rs
@@ -1,4 +1,4 @@
-use nalgebra::base::Vector3;
+use nalgebra::base::{Vector3, Vector4};
 
 fn generate_binomial_coefficients(n: usize) -> Vec<u16> {
   if n == 0 {
@@ -20,7 +20,7 @@ fn generate_binomial_coefficients(n: usize) -> Vec<u16> {
   }
 }
 
-fn bezier(t: f32, points: &Vec<Vector3<f32>>) -> Vector3<f32> {
+fn bezier(t: f32, points: &Vec<Vector3<f32>>) -> Vector4<f32> {
   let n = points.len();
 
   let t = f32::clamp(t, 0., 1.);
@@ -39,39 +39,43 @@ fn bezier(t: f32, points: &Vec<Vector3<f32>>) -> Vector3<f32> {
     sum
   };
 
-  let mut res = Vector3::zeros();
+  let mut res = Vector4::zeros();
 
   res[0] = bez(0);
   res[1] = bez(1);
   res[2] = bez(2);
+  res[3] = 1.;
 
   res
 }
 
-pub fn sample_bezier(sample_size: u16, points: &Vec<Vector3<f32>>) -> Vec<Vector3<f32>> {
-  let sample_rate = 1. / sample_size as f32;
+pub fn sample_bezier(sample_size: u16, segments: &Vec<Vec<Vector3<f32>>>) -> Vec<Vector4<f32>> {
+  let sample_size = f32::ceil(sample_size as f32 / segments.len() as f32);
+  let sample_rate = 1. / sample_size;
   let mut samples = vec![];
 
-  for i in 0..sample_size {
-    let sample = bezier(i as f32 * sample_rate, &points);
-    samples.push(sample);
+  for points in segments {
+    for i in 0..sample_size as usize {
+      let sample = bezier(i as f32 * sample_rate, &points);
+      samples.push(sample);
+    }
   }
 
   samples
 }
 
-pub fn generate_bezier_segments() -> Vec<Vec<Vector3<f32>>> {
-  let p0 = Vector3::new(60.5, -200., -64.);
-  let p1 = Vector3::new(60.5, -200., -64.);
-  let p2 = Vector3::new(60.5, -160., -90.);
-  let p3 = Vector3::new(60.5, -160., -90.);
-  let p4 = Vector3::new(60.5, -160., -90.);
-  let p5 = Vector3::new(60.5, -160., 0.);
-  let p6 = Vector3::new(60.5, -160., 0.);
-  let p7 = Vector3::new(60.5, -140., 0.);
-  let p8 = Vector3::new(60.5, -140., 90.);
-  let p9 = Vector3::new(60.5, -140., 90.);
-  let p10 = Vector3::new(60.5, -200., 64.);
-  let p11 = Vector3::new(60.5, -200., 64.);
-  vec![vec![p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11]]
+pub fn generate_bezier_segments_lf() -> Vec<Vec<Vector3<f32>>> {
+  let p0 = Vector3::new(40., -200., 100.);
+  let p1 = Vector3::new(20., -200., 100.);
+  let p2 = Vector3::new(0., -160., 100.);
+  let p3 = Vector3::new(0., -160., 100.);
+  let p4 = Vector3::new(0., -160., 100.);
+  let p5 = Vector3::new(100., -160., 100.);
+  let p6 = Vector3::new(100., -160., 100.);
+  let p7 = Vector3::new(100., -140., 100.);
+  let p8 = Vector3::new(200., -140., 100.);
+  let p9 = Vector3::new(200., -140., 100.);
+  let p10 = Vector3::new(160., -200., 100.);
+  let p11 = Vector3::new(140., -200., 100.);
+  vec![vec![p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p0]]
 }
